fad2b34ae1f0818d85fd32adbc1a6c9ae2aee3cf
Staff GVA <GVA@Sach-Australia.local>
2019-11-14 06:31:45 +1100

    Mongoose, but broken - not connecting to DB

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-import express from 'express';
-import routes from './src/routes/crmRoutes';
 require('dotenv').config();
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import routes from './src/routes/crmRoutes';
 
 const app = express();
 const PORT = 4000;
 const DB_PASS = process.env.DB_PASS;
+
+// Mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect(`mongodb+srv://goo:${DB_PASS}@coder-g8zwo.gcp.mongodb.net/test?retryWrites=true&w=majority`, {
+	useMongoClient: true
+});
+
+// Body Parser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 routes(app);
 
