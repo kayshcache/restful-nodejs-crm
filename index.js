9ee335810997d3070ade4777bece5a1b60661213
Kaysh <kaysh@cache.la>
2019-11-14 11:10:09 +1100

    MongoDB connection success

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import routes from './src/routes/crmRoutes';
+
+dotenv.config();
 
 const app = express();
 const PORT = 4000;
@@ -11,7 +13,8 @@ const DB_PASS = process.env.DB_PASS;
 // Mongoose connection
 mongoose.Promise = global.Promise;
 mongoose.connect(`mongodb+srv://goo:${DB_PASS}@coder-g8zwo.gcp.mongodb.net/test?retryWrites=true&w=majority`, {
-	useMongoClient: true
+	useUnifiedTopology: true,
+	useNewUrlParser: true,
 });
 
 // Body Parser setup
